626a4429d7f51c4988d9587f4e39f29721525e86
Igor <68248740+richard96292@users.noreply.github.com>
2022-05-13 16:48:20 +0300

    Update mangohud script

--- scripts/post-archinstall.sh
+++ scripts/post-archinstall.sh
@@ -564,7 +564,7 @@ for choice in ${choicesGaming}; do
         sudo pacman -S --needed vkd3d lib32-vkd3d giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader lib32-gst-plugins-base lib32-gst-plugins-good lib32-libcups --noconfirm --needed
         ;;
     mangohud)
-        sudo -u ${username} paru -S mangohud lib32-mangohud --noconfirm --needed
+        sudo -u ${username} paru -S mangohud-common-x11 mangohud-x11 lib32-mangohud-x11 --noconfirm --needed
         curl --create-dirs --output /home/${username}/.config/MangoHud/MangoHud.conf https://raw.githubusercontent.com/richard96292/ALIS/master/configs/MangoHud.conf
         curl --create-dirs --output /home/${username}/.var/app/com.valvesoftware.Steam/config/MangoHud/MangoHud.conf https://raw.githubusercontent.com/richard96292/ALIS/master/configs/MangoHud.conf
         ;;
