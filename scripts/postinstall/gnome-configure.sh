c0fc83b6cabf750f36e17ab4409edc0f122f56eb
Igor <68248740+richard96292@users.noreply.github.com>
2022-06-02 20:54:38 +0300

    Update folders

--- scripts/postinstall/gnome-configure.sh
+++ scripts/postinstall/gnome-configure.sh
@@ -9,13 +9,10 @@ set -xe
 sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'chromium.desktop', 'thunderbird.desktop', 'telegramdesktop.desktop', 'org.telegram.desktop.desktop', 'com.github.taiko2k.tauonmb.desktop', 'com.discordapp.Discord.desktop', 'discord.desktop', 'com.spotify.Client.desktop', 'com.valvesoftware.Steam.desktop', 'steam.desktop', 'net.lutris.Lutris.desktop', 'bitwarden.desktop', 'org.keepassxc.KeePassXC.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop', 'code-oss.desktop']"
 
 # application folders
-sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.app-folders folder-children "['space-wasters', 'tools']"
-
-sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/space-wasters/ name 'Space Wasters'
-sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/space-wasters/ apps "['yelp.desktop', 'cmake-gui.desktop', 'hplip.desktop', 'libreoffice-startcenter.desktop', 'libreoffice-math.desktop', 'libreoffice-base.desktop', 'libreoffice-draw.desktop', 'lstopo.desktop', 'electron.desktop', 'electron18.desktop', 'electron17.desktop', 'electron16.desktop', 'electron15.desktop', 'electron14.desktop', 'electron13.desktop', 'hp-uiscan.desktop', 'avahi-discover.desktop', 'bvnc.desktop', 'bssh.desktop', 'nm-connection-editor.desktop', 'org.gnome.Epiphany.desktop', 'qvidcap.desktop', 'qv4l2.desktop', 'qdbusviewer.desktop', 'linguist.desktop', 'designer.desktop', 'assistant.desktop']"
+sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.app-folders folder-children "['tools']"
 
 sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/tools/ name 'Tools'
-sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/tools/ apps "['winetricks.desktop', 'nvidia-settings.desktop', 'timeshift-gtk.desktop', 'vim.desktop', 'ca.desrt.dconf-editor.desktop', 'tlpui.desktop']"
+sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/tools/ apps ['winetricks.desktop', 'nvidia-settings.desktop', 'cmake-gui.desktop', 'timeshift-gtk.desktop', 'vim.desktop', 'ca.desrt.dconf-editor.desktop', 'tlpui.desktop', 'qt5ct.desktop', 'qt6ct.desktop']
 
 # mutter
 sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.mutter center-new-windows true
@@ -47,13 +44,13 @@ sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desk
 sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.sound event-sounds false
 
 # set monospace font to jetbrains mono
-sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface font-name 'Ubuntu 11'
+sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface font-name 'Noto Sans 11'
 
-sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface document-font-name 'Sans 11'
+sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 11'
 
 sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono 10'
 
-sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Ubuntu Bold 11'
+sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Noto Sans 11'
 
 # disable animations
 sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface enable-animations false
@@ -89,3 +86,9 @@ sudo -u ${username} dbus-launch --exit-with-session gsettings set org.gnome.mutt
 echo "[org.gnome.desktop.peripherals.mouse]" | sudo tee /usr/share/glib-2.0/schemas/69_acceleration.gschema.override
 echo "accel-profile='flat'" | sudo tee -a /usr/share/glib-2.0/schemas/69_acceleration.gschema.override
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas
+
+# hide desktop entries
+# echo "[Desktop Entry]
+# Hidden=true" > /tmp/1
+
+# find /usr -name "*lsp_plug*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
