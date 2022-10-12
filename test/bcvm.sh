dd08f2f57a2457d9b28d92b69f16e1326d675c40
Igor <68248740+richard96292@users.noreply.github.com>
2022-10-12 18:43:56 +0300

    Add vm creation scripts

---
+++ test/bcvm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+virt-install --connect qemu:///system --memory 4096 --vcpus 4 --osinfo archlinux --cdrom ~/Downloads/archlinux-2022.10.01-x86_64.iso --disk size=40
+
+sudo virsh destroy archlinux && sudo virsh undefine archlinux
+
+sudo virsh pool-refresh default
